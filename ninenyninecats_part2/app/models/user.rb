309401b5f680b40efd0ac888ed4e2a570fab3817
alejandro <alejandroweil13@gmail.com>
2021-02-08 14:15:34 -0500

    Made users table and started authentication methods

---
+++ ninenyninecats_part2/app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+
+    after_initialize :ensure_session_token
+
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+end
