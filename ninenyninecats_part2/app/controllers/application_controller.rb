1ee83967a824486309bf7f4e184692ded4b27e98
Christian <cwooddel@u.rochester.edu>
2021-02-08 15:37:58 -0500

    Save ApplicationController

--- ninenyninecats_part2/app/controllers/application_controller.rb
+++ ninenyninecats_part2/app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
             session[:session_token] = user.reset_session_token!
         end
                     
+        def current_user
+            @current_user ||= User.find_by(session_token: session[:session_token])
+        end 
+
+        
 
         
 
